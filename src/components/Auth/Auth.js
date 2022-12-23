e801185d2a945462293e79f99319f8e58734cee9
Andrey Mekeda <blestan5@gmail.com>
2022-12-24 00:12:31 +0200

    auth class

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,30 +1,31 @@
+import { Component } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import styles from "./Auth.module.scss";
 
-function Auth({toggleLogin}) {
-  return (
-    <section className="auth">
-      <div className={`${styles.authContainer} container`}>
-        <div className={styles.authInner}>
-          <Tabs
-            defaultActiveKey="login"
-            id="uncontrolled-tab-example"
-            className="mb-3"
-          >
-            <Tab eventKey="register" title="Регістрація">
-              <Register toggleLogin={toggleLogin}/>
-            </Tab>
-            <Tab eventKey="login" title="Логін">
-              <Login toggleLogin={toggleLogin} />
-            </Tab>
-          </Tabs>
+export default class Auth extends Component {
+  render() {
+    return (
+      <section className="auth">
+        <div className={`${styles.authContainer} container`}>
+          <div className={styles.authInner}>
+            <Tabs
+              defaultActiveKey="login"
+              id="uncontrolled-tab-example"
+              className="mb-3"
+            >
+              <Tab eventKey="register" title="Регістрація">
+                <Register toggleLogin={this.propstoggleLogin}/>
+              </Tab>
+              <Tab eventKey="login" title="Логін">
+                <Login toggleLogin={this.props.toggleLogin} />
+              </Tab>
+            </Tabs>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
-
-export default Auth;
