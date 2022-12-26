b1d920d32e1f5ec535b3aa5ab454852d0151b383
Andrey Mekeda <blestan5@gmail.com>
2022-12-27 00:47:30 +0200

    AuthFormInner

--- src/components/AuthFormInner/AuthFormInner.js
+++ src/components/AuthFormInner/AuthFormInner.js
@@ -1,22 +1,27 @@
+import { Component } from "react";
 import Form from "react-bootstrap/Form";
 import styles from './AuthFormInner.module.scss';
 
-function AuthFormInner({loginField, passField, error}) {
-
-  return (
-    <>
-      {error !== '' ? <p className={styles.error}>{error}</p> : null}
-      <Form.Group className="mb-3">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="text" placeholder="Enter login" {...loginField} />
-      </Form.Group>
-
-      <Form.Group className="mb-3" >
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" {...passField}/>
-      </Form.Group>
-    </>
-  );
+// ({loginField, passField, error})
+class AuthFormInner extends Component {
+  render() {
+    const {loginField, useLoginField} = this.props.loginField;
+    const {passField, usePassField} = this.props.passField;
+    return (
+      <>
+        {/* {error !== '' ? <p className={styles.error}>{error}</p> : null} */}
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="text" placeholder="Enter login" value={loginField} onChange={(e)=>useLoginField(e.target.value)} />
+        </Form.Group>
+  
+        <Form.Group className="mb-3" >
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={passField} onChange={(e)=>usePassField(e.target.value)}/>
+        </Form.Group>
+      </>
+    );
+  }
 }
 
 export default AuthFormInner;
